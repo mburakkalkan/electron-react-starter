73bb5a7205d41fcf940db4f4474d9c78ddfef310
mburakkalkan <mburakkalkan@gmail.com>
2020-04-27 22:06:19 +0300

    kill development server process before quit

--- public/electron.js
+++ public/electron.js
@@ -1,28 +1,32 @@
-const electron = require("electron");
-const app = electron.app;
-const BrowserWindow = electron.BrowserWindow;
-
+const { app, BrowserWindow } = require('electron');
 const path = require("path");
 const isDev = require("electron-is-dev");
 
 let mainWindow;
 
 require("update-electron-app")({
-  repo: "kitze/react-electron-example",
+  repo: "mburakkalkan/electron-react-starter",
   updateInterval: "1 hour"
 });
 
 function createWindow() {
-  mainWindow = new BrowserWindow({ width: 900, height: 680, webPreferences: { nodeIntegration: true }});
+  mainWindow = new BrowserWindow({
+    width: 900,
+    height: 680, 
+    show: false,
+    icon: path.join(__dirname, "favicon.ico"),
+    webPreferences: { nodeIntegration: true }
+  });
+  mainWindow.once("ready-to-show", () => { mainWindow.show(); });
+  mainWindow.on("closed", () => (mainWindow = null));
   mainWindow.loadURL(
     isDev
       ? "http://localhost:3000"
       : `file://${path.join(__dirname, "../build/index.html")}`
   );
-  mainWindow.on("closed", () => (mainWindow = null));
 }
 
-app.on("ready", createWindow);
+app.whenReady().then(createWindow);
 
 app.on("window-all-closed", () => {
   if (process.platform !== "darwin") {
@@ -35,3 +39,19 @@ app.on("activate", () => {
     createWindow();
   }
 });
+
+// kill development server process before quit
+if (isDev) {
+  const find = require("find-process");
+  let devServerPid = null;
+  find("port", 3000).then(list => {
+    if(list[0]) {
+      devServerPid = list[0].pid;
+    }
+  });
+  app.on("will-quit", e => {
+    if (devServerPid) {
+      process.kill(devServerPid, "SIGINT");
+    }
+  });
+}
